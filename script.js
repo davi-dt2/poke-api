279f45fc826605f74b58cffc986eacf2776bd587
Davi <136943274+davi-dt2@users.noreply.github.com>
2024-11-03 21:39:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 const pokeCount = 151;
-const colors = { fire: '#FDDFDF', grass: '#DEFDE0', electric: '#FCF7DE', water: '#DEF3FD',};
+const colors = {fire: '#FDDFDF', grass: '#DEFDE0',electric: '#FCF7DE', water: '#DEF3FD', ground: '#f4e7da', rock: '#d5d5d4', fairy: '#fceaff', poison: '#98d7a5', bug: '#f8d5a3', dragon: '#97b3e6', psychic: '#eaeda1', flying: '#F5F5F5', fighting: '#E6E0D4', normal: '#F5F5F5', ice: '#d0f0fd', steel: '#d5d5e5', ghost: '#a393eb', dark: '#a9a9a9'};
 const mainTypes = Object.keys(colors);
+
 
 async function fetchPoke(id) {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
